01b1ca4e92199b2e85d55719f4655b4c3c57334f
Kamal Shah <kkamal714@gmail.com>
2021-03-13 23:04:34 +0530

    [Kamal] | Fixes Styling

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import CustomHeading from './CustomHeading';
 
-const CountDown = ({ isPaused }) => {
+const CountDown = ({ isPaused, onProgress, minutes }) => {
   const minutesToMillisecond = (minute) => minute * 1000 * 60;
-  const [milliSeconds, setMilliSeconds] = useState(minutesToMillisecond(20));
+  const [milliSeconds, setMilliSeconds] = useState(0);
   const interval = React.useRef();
-  const minutes = Math.floor(milliSeconds / 1000 / 60) % 60;
+  const minutesFormat = Math.floor(milliSeconds / 1000 / 60) % 60;
   const seconds = Math.floor(milliSeconds / 1000) % 60;
   const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
   const countDown = () => {
     setMilliSeconds((time) => {
       if (time === 0) {
-        // more stuff do
         return time;
       }
       const timeLeft = time - 1000;
+      onProgress(timeLeft / milliSeconds);
       return timeLeft;
     });
   };
@@ -27,9 +27,17 @@ const CountDown = ({ isPaused }) => {
     interval.current = setInterval(countDown, 1000);
     return () => clearInterval(interval.current);
   }, [isPaused]);
+
+  React.useEffect(() => {
+    if (minutes) {
+      setMilliSeconds(minutesToMillisecond(minutes));
+    }
+  }, [minutes]);
   return (
     <View style={styles.main}>
-      <CustomHeading text={`${formatTime(minutes)}:${formatTime(seconds)}`} />
+      <CustomHeading
+        text={`${formatTime(minutesFormat)}:${formatTime(seconds)}`}
+      />
     </View>
   );
 };
@@ -41,5 +49,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#8D3DAF',
     padding: 20,
     borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40,
+    marginTop: 50,
   },
 });
